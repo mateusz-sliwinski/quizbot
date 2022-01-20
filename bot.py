f5e182a3314f096f649df9b23fc26d88da0c23b4
Mati1432 <mateuszsliwinski9@gmail.com>
2022-01-20 14:12:33 +0100

    initial commit

--- bot.py
+++ bot.py
@@ -1,13 +1,53 @@
 import discord
+import requests
+import json
+import asyncio
+
 
 client = discord.Client()
+
+@client.event
+def get_question():
+    qs = ''
+    id = 1
+    answer = 0
+    response = requests.get("https://mysterious-fjord-93163.herokuapp.com/api/random/")
+    json_data = json.loads(response.text)
+    qs += "Question: \n"
+    qs += json_data[0]['title'] + "\n"
+
+    for item in json_data[0]['answer']:
+        qs += str(id) + ". " + item['answer'] + "\n"
+
+        if item['is_correct']:
+            answer = id
+
+        id += 1
+
+    return (qs, answer)
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('hello'):
-        await message.channel.send('hello, I am a bot')
+    if message.content.startswith('$question'):
+
+        qs, answer = get_question()
+        await message.channel.send(qs)
+
+        def check(m):
+            return m.author == message.author and m.content.isdigit()
+
+        try:
+            guess = await client.wait_for('message', check=check, timeout=2.0)
+        except asyncio.TimeoutError:
+            return await message.channel.send('Za długo ByczQ')
+
+        if int(guess.content) == answer:
+            await message.channel.send('Masz racje!')
+        else:
+            await message.channel.send('Oops. nie tym razem')
 
 client.run('')
--- quiz/models.py
+++ quiz/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from consts import LEVEL
+from .consts import LEVEL
 
 
 class Question(models.Model):
